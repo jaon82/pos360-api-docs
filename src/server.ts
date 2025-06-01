c04de3d0bf22d0f08c4c4492b88a7689bf8f8367
Jean Nunes <jean.nunes@uzzipay.com>
2025-06-01 13:12:33 -0400

    Setup Fastify app

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastify } from "fastify";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Example API Docs",
+      version: "1.0.0",
+    },
+  },
+});
+
+app.get("/spec.json", () => app.swagger());
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
