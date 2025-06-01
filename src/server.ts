6c5f62ddcb35b40f14c0bbceafe705a2f272ee18
Jean Nunes <jean.nunes@uzzipay.com>
2025-06-01 13:46:33 -0400

    Add users creation documentation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { fastifyCors } from "@fastify/cors";
 import { fastifySwagger } from "@fastify/swagger";
 import scalarUI from "@scalar/fastify-api-reference";
 import { fastify } from "fastify";
+import { createUserRoute } from "./routes/create-user-route";
 import { getUsersRoute } from "./routes/get-users-route";
 
 const app = fastify();
@@ -20,6 +21,7 @@ app.register(fastifySwagger, {
 });
 
 app.register(getUsersRoute);
+app.register(createUserRoute);
 
 app.get("/openapi.json", () => app.swagger());
 
